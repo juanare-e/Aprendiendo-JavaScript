cebc1ce9d15a4caa87bfa18519cba76537346ed5
juan-arenas <arenasjuansot@gmail.com>
2026-07-16 13:02:29 -0500

    Create quiz_mod_uno.js

---
+++ Modulo 1/quiz_mod_uno.js
@@ -0,0 +1,32 @@
+//Durante el modulo 1, nos hacen un pequeño quiz sobre lo aprendido, pero ese quiz no es la prueba final.
+
+//1. Use console.log to output your full name to the console.
+console.log("Juan Esteban Arenas S");
+
+//2. Output your year birthday.
+console.log("27-03");
+
+//3.  Try again to output your year of birth, this time passing the date without the quotes.
+console.log("27-03");
+
+//4. We can pass several arguments to console.log separated by commas, e.g.:
+// console.log("abc", "def", "ghi");
+//Output information about yourself to the console in the format: Name Surname (Year) e.g. Mary Stuart (1542).
+// -giving all the information as one argument;
+// -giving the name, surname, and year as separate arguments.
+
+console.log ("Juan Esteban Arenas S (27-03)");
+console.log("Juan Esteban", "Arenas S.", "27", "03");
+
+//5. Output the same information (name, surname, year) to the console, not side by side, but on consecutive lines.
+console.log("Juan Esteban")
+console.log("27")
+console.log("03")
+
+
+//6. A string can be concatenated using the + sign, for example "abc" + "def" will be treated as "abcdef".
+// Try writing your name, surname, and year of birth on one line again, this time not separated by commas, but by + signs.
+console.log("Juan Esteban" + "Arenas" + "(27" + "-" + "03)");
+
+//7.  Put spaces in the appropriate places, so that when displayed, you get the same effect as in Question 4.
+console.log("Juan Esteban" + "Arenas" + "(2703)");
